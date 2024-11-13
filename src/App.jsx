2e030df0bd7033ec33e00468356091cbd1a392ed
Maxim <besstrakhov.m@gmail.com>
2024-11-13 20:30:48 +0200

    add change name

--- src/App.jsx
+++ src/App.jsx
@@ -19,11 +19,21 @@ function App() {
     const changeAvatar = url => {
         setUser(prevState => ({...prevState, avatar: url || prevState.avatar}));
     }
+    const changeName = (newName) => {
+        setUser(prevState=>({...prevState,name: newName||prevState.name}));
+    }
+    const changeCount = (stat,action) => {
+        switch (action) {
+            case '+':
+                setUser(prev=>prev+1);
+
+        }
+    }
 
     return (
         <div className={'app'}>
             <TwitterContext.Provider value={{
-                user, stats, changeAvatar
+                user, stats, changeAvatar, changeName,
             }}>
                 <Navigation/>
                 <Body/>
--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -2,13 +2,18 @@ import {useContext} from "react";
 import {TwitterContext} from "../utils/context.js";
 
 const Avatar = ({size}) => {
-    const {user, changeAvatar} = useContext(TwitterContext);
+    const {user, changeAvatar,changeName} = useContext(TwitterContext);
 
     return (
         <img
             onClick={() => {
                 const url = prompt('Enter new avatar url');
                 changeAvatar(url);
+            }}
+            onContextMenu={(e) => {
+                e.preventDefault();
+                const newName=prompt('Enter name');
+                changeName(newName);
             }}
             className={`user-avatar ${size ?? ''}`}
             src={user.avatar} alt={user.name}
